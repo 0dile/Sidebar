759c8054fdb95adf80048244a9641cc286c1a9fc
Odile <ayodeodiletchalla@gmail.com>
2025-02-19 16:41:39 +0100

    sidebar

---
+++ side.js
@@ -0,0 +1,9 @@
+
+
+let myspan = document.querySelector(".icone")
+let myliens = document.querySelector(".liens")
+
+myspan.addEventListener("click", (e) => {
+    myspan.classList.toggle("iconehover")
+    myliens.classList.toggle("linshover")
+})
